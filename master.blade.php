1727fb21fe85aa151d2c50602b0b834c42ac1739
abusayed310278 <83999377+abusayed310278@users.noreply.github.com>
2024-10-16 03:03:47 +0600

    Add files via upload

---
+++ master.blade.php
@@ -0,0 +1,239 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+    <meta name="csrf-token" id="csrf-token" content="{{ csrf_token() }}">
+    <title>Dashboard </title>
+
+    <!-- General CSS Files -->
+
+    <link rel="stylesheet" href="{{ asset('admin/assets/modules/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/modules/fontawesome/css/all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/modules/select2/dist/css/select2.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/modules/summernote/summernote-bs4.css') }}">
+
+
+
+    {{--    toaster css--}}
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/toastr.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/bootstrap-iconpicker.css') }}">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('admin/assets/css/components.css') }}">
+    <!-- Start GA -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-94034622-3"></script>
+
+
+
+    <script>
+/*        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+
+        gtag('js', new Date());
+
+        gtag('config', 'UA-94034622-3');*/
+
+        var pusherKey = "{{ config('settings.pusher_key') }}";
+        var pusherCluster = "{{ config('settings.pusher_cluster') }}";
+        var loggedInUserId = "{{ auth()->user()->id ?? '' }}";
+
+    </script>
+
+    {{--    <link rel="stylesheet" href="//cdn.datatables.net/2.1.7/css/dataTables.dataTables.min.css">--}}
+    <link rel="stylesheet" href="//cdn.datatables.net/1.11.3/css/jquery.dataTables.min.css">
+
+    <!-- /END GA -->
+    @vite(['resources/js/app.js','resources/js/admin.js'])
+</head>
+
+<body>
+<div id="app">
+    <div class="main-wrapper main-wrapper-1">
+        <div class="navbar-bg"></div>
+
+        @include('admin.layouts.sidebar');
+
+        <!-- Main Content -->
+        <div class="main-content">
+
+            @yield('content')
+
+        </div>
+        <footer class="main-footer">
+            <div class="footer-left">
+                Copyright &copy; 2024
+                <div class="bullet"></div>
+                Design By <a href="https://nauval.in/">Sayed</a>
+            </div>
+            <div class="footer-right">
+
+            </div>
+        </footer>
+    </div>
+</div>
+
+<!-- General JS Scripts -->
+<script src="{{ asset('admin/assets/modules/jquery.min.js') }}"></script>
+<script src="{{ asset('admin/assets/modules/popper.js') }}"></script>
+<script src="{{ asset('admin/assets/modules/tooltip.js') }}"></script>
+<script src="{{ asset('admin/assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+<script src="{{ asset('admin/assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+<script src="{{ asset('admin/assets/js/stisla.js') }}"></script>
+
+<script src="{{ asset('admin/assets/modules/summernote/summernote-bs4.js') }}"></script>
+
+{{-- toastr jss--}}
+<script src="{{ asset('admin/assets/js/toastr.min.js') }}"></script>
+<script src="{{ asset('admin/assets/js/bootstrap-iconpicker.bundle.min.js') }}"></script>
+<script src="{{ asset('admin/assets/modules/upload-preview/assets/js/jquery.uploadPreview.min.js') }}"></script>
+<script src="//cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+<!-- Template JS File -->
+
+<script src="{{ asset('admin/assets/js/scripts.js') }}"></script>
+<script src="{{ asset('admin/assets/js/custom.js') }}"></script>
+
+{{--<script src="//code.jquery.com/jquery-3.6.0.min.js"></script>--}}
+
+
+
+{{-- table jquery--}}
+{{--<script src="//cdn.datatables.net/2.1.7/css/dataTables.dataTables.min.css"></script>--}}
+
+
+
+
+<!--show dynamic validation message js-->
+
+<script>
+    toastr.options.progressBar = true;
+
+    @if($errors->any())
+    @foreach($errors->all() as $error)
+    toastr.error("{{ $error }}");
+    @endforeach
+    @endif
+</script>
+
+<script>
+    $.uploadPreview({
+        input_field: "#image-upload",   // Default: .image-upload
+        preview_box: "#image-preview",  // Default: .image-preview
+        label_field: "#image-label",    // Default: .image-label
+        label_default: "Choose File",   // Default: Choose File
+        label_selected: "Change File",  // Default: Change File
+        no_label: false,                // Default: false
+        success_callback: null          // Default: null
+    });
+
+    {{--            ajax token--}}
+
+    // $.ajaxSetup({
+    //     headers: {
+    //         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    //     }
+    // });
+
+
+    //this is sweet alert code
+    $(document).ready(function () {
+
+        $('body').on('click', '.delete-item', function (e) {
+
+            e.preventDefault();
+            let url = $(this).attr('href');
+            console.log(url);
+
+
+            Swal.fire({
+                title: "Are you sure?",
+                text: "You won't be able to revert this!",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Yes, delete it!"
+            }).then((result) => {
+                if (result.isConfirmed) {
+
+                    $.ajax({
+                        method: 'DELETE',
+                        url: url,
+                        data:{ _token:"{{ csrf_token() }}" },
+                        success: function (response) {
+                            if (response.status === 'success') {
+
+                                toastr.success(response.message)
+
+                                window.location.reload();
+
+                                // $('#slider-table').DataTable().draw();
+
+                            } else if (response.status === 'error') {
+                                toastr.error(response.message)
+
+                            }
+                        },
+                        error: function (error) {
+                            console.error(error)
+                        }
+                    })
+
+                }
+            });
+        })
+    });
+
+
+</script>
+
+@stack('scripts')
+</body>
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
